fddcd80e9a27027b4024ae2a96527e47c7e128ae
mohamedadel2323 <mohamedadel2323m@gmail.com>
2023-04-30 12:31:40 +0300

    added Test.txt

---
+++ HelloIti.java
@@ -0,0 +1,11 @@
+	package mypkg;
+
+import java.util.Scanner;
+
+class HelloJava {
+	public static void main(String[] args){
+		if(args.length != 0)
+			for(int i =0 ; i < args.length ; i++)
+				System.out.print(args[i]+" ");
+	}
+}
