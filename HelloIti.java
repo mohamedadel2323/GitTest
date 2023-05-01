78265fc139a76884906f62b556ef20e880f42131
mohamedadel2323 <mohamedadel2323m@gmail.com>
2023-05-01 12:48:18 +0300

    update1

--- HelloIti.java
+++ HelloIti.java
@@ -1,7 +1,7 @@
 	package mypkg;
 
 import java.util.Scanner;
-//update in same line to make conflict
+//update1 in same line to make conflict
 class HelloJava {
 	public static void main(String[] args){
 		if(args.length != 0)
